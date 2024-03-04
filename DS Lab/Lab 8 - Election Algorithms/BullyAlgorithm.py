be8b29f2c1c537e6f76aafb110f431abf9e4ae91
Vishal Puneyani <91496123+vishaal-21@users.noreply.github.com>
2024-03-04 16:28:17 +0530

    DSL8 Q1 Updated

--- DS Lab/Lab 8 - Election Algorithms/BullyAlgorithm.py
+++ DS Lab/Lab 8 - Election Algorithms/BullyAlgorithm.py
@@ -1,16 +1,33 @@
 import random
+import threading
 
-class Process:
+class Bully:
     processes={}
 
-    def __init__(self,index,id):
+    def __init__(self,index,id,ids):
         self.index=index
         self.process_id=id
-        self.status=1
         self.processes[index]=id
+        
+        self.process_ids=ids
+        self.coord_id=self.process_ids.index(max(self.process_ids))
+        self.coord=max(self.process_ids)
 
     def display(self):
-        print("Process "+str(self.index)+" ID "+str(self.process_id)+" Status "+str(self.status))
+        print(f"Process {self.index} ID {self.process_id} Coordinator : Process {self.coord_id} => {self.coord}")
+        
+    def startElection(self,elec):        
+        if 0 <= elec < len(self.process_ids) and elec != self.coord_id:
+            contender_ids = [pid for pid in self.process_ids if pid > self.process_ids[elec] and pid!=self.coord]
+            print("Contender processes:", contender_ids)
+        else:
+            print("Invalid process number for election")
+            
+        if contender_ids:
+            self.coord=max(contender_ids)
+            self.coord_id=contender_ids.index(self.coord)
+            return self.coord,self.coord_id
+        
 
 def main():
     n=int(input("Enter number of processes : "))
@@ -18,20 +35,24 @@ def main():
     print(ids)
 
     objects=[]
+    new_co=new_co_id=0
     
     for i in range(n):
-        obj=Process(i,ids[i])
+        obj=Bully(i,ids[i],ids)
         objects.append(obj)
         objects[i].display()
-
-    co_ord = max(Process.processes, key=lambda k: Process.processes[k])
-    objects[co_ord].status=0
-    print("\nCo-ordinator is Process %d\n" %co_ord)
-
+        
+    elec=int(input(f"\nEnter process number who sends ELECTION msg (0 to {n-1} except {objects[0].coord_id}) : "))
+    
     for i in range(n):
+        if objects[i].index==elec:
+            new_co,new_co_id=objects[i].startElection(elec)
+         
+    for i in range(n):
+        objects[i].coord=new_co
+        objects[i].coord_id=new_co_id   
         objects[i].display()
-
-    elec=int(input("Enter process number who sends ELECTION msg : "))
+        
 
 if __name__=="__main__":
     main()
